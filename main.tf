1dff0d851f474c68ae75ea395f483e2ebf363623
Joe Dillig <40437881+joe-at-cp@users.noreply.github.com>
2020-11-18 12:17:37 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,11 +3,11 @@
 ##############################################################################
 
 provider "ibm" {
-  ibmcloud_api_key   = "${var.ibmcloud_api_key}"
+  ibmcloud_api_key   = var.ibmcloud_api_key
   generation         = 2
-  region             = "${var.VPC_Region}"
+  region             = var.VPC_Region
   ibmcloud_timeout   = 300
-  resource_group     = "${var.Resource_Group}"
+  resource_group     = var.Resource_Group
 }
 
 ##############################################################################
@@ -19,13 +19,8 @@ variable "VPC_Region" {
   description = "The region where the VPC, networks, and Check Point VSI will be provisioned."
 }
 
-variable "VPC_Zone" {
-  default     = ""
-  description = "The zone where the VPC, networks, and Check Point VSI will be provisioned."
-}
-
 variable "Resource_Group" {
-  default     = "Default"
+  default     = ""
   description = "The resource group that will be used when provisioning the Check Point VSI. If left unspecififed, the account's default resource group will be used."
 }
 
@@ -89,35 +84,39 @@ variable "ibmcloud_api_key" {
 ##############################################################################
 
 data "ibm_is_subnet" "cp_subnet1" {
-  identifier = "${var.Subnet_ID}"
+  identifier = var.Subnet_ID
 }
 
 data "ibm_is_region" "region" {
-  name = "${var.VPC_Region}"
+  name = var.VPC_Region
 }
 
-data "ibm_is_zone" "zone" {
-  name   = "${var.VPC_Zone}"
-  region = "${data.ibm_is_region.region.name}"
-}
+#data "ibm_is_zone" "zone" {
+#  name   = var.VPC_Zone
+#  region = data.ibm_is_region.region.name
+#}
 
 data "ibm_resource_group" "rg" {
-  name = "${var.Resource_Group}"
+  name = var.Resource_Group
 }
 
 ##############################################################################
 # Create Custom Image
 ##############################################################################
 
+# Generating random ID
+resource "random_uuid" "test" { }
+
 locals {
   image_url    = "cos://${var.VPC_Region}/checkpoint-${var.VPC_Region}/Check_Point_${var.CP_Version}_Cloudguard_Security_Management.qcow2"
 }
 
 resource "ibm_is_image" "cp_mgmt_custom_image" {
-  href             = "${local.image_url}"
-  name             = "${var.VNF_CP-MGMT_Instance}"
+  depends_on       = [random_uuid.test]
+  href             = local.image_url
+  name             = "${var.VNF_CP-MGMT_Instance}-${substr(random_uuid.test.result,0,8)}"
   operating_system = "centos-7-amd64"
-  resource_group   = "${data.ibm_resource_group.rg.id}"
+  resource_group   = data.ibm_resource_group.rg.id
 
   timeouts {
     create = "30m"
@@ -126,19 +125,19 @@ resource "ibm_is_image" "cp_mgmt_custom_image" {
 }
 
 data "ibm_is_image" "cp_mgmt_custom_image" {
-  name = "${ibm_is_image.cp_mgmt_custom_image.name}"
+  name = ibm_is_image.cp_mgmt_custom_image.name
 }
 
 data "ibm_is_ssh_key" "cp_ssh_pub_key" {
-  name = "${var.SSH_Key}"
+  name = var.SSH_Key
 }
 
 data "ibm_is_instance_profile" "vnf_profile" {
-  name = "${var.VNF_Profile}"
+  name = var.VNF_Profile
 }
 
 data "ibm_is_vpc" "cp_vpc" {
-  name = "${var.VPC_Name}"
+  name = var.VPC_Name
 }
 
 ##############################################################################
@@ -146,23 +145,23 @@ data "ibm_is_vpc" "cp_vpc" {
 ##############################################################################
 
 resource "ibm_is_security_group" "ckp_security_group" {
-    name = "${var.VNF_Security_Group}"
-    vpc = "${data.ibm_is_vpc.cp_vpc.id}"
-    resource_group = "${data.ibm_resource_group.rg.id}"
+    name = var.VNF_Security_Group
+    vpc = data.ibm_is_vpc.cp_vpc.id
+    resource_group = data.ibm_resource_group.rg.id
 }
 
 #Egress All Ports
 resource "ibm_is_security_group_rule" "allow_egress_all" {
-  depends_on = ["ibm_is_security_group.ckp_security_group"]
-  group      = "${ibm_is_security_group.ckp_security_group.id}"
+  depends_on = [ibm_is_security_group.ckp_security_group]
+  group      = ibm_is_security_group.ckp_security_group.id
   direction  = "outbound"
   remote     = "0.0.0.0/0"
 }
 
 #Ingress 22 Port
 resource "ibm_is_security_group_rule" "allow_22" {
-  depends_on = ["ibm_is_security_group.ckp_security_group"]
-  group      = "${ibm_is_security_group.ckp_security_group.id}"
+  depends_on = [ibm_is_security_group.ckp_security_group]
+  group      = ibm_is_security_group.ckp_security_group.id
   direction  = "inbound"
   remote     = "0.0.0.0/0"
   tcp {
@@ -173,8 +172,8 @@ resource "ibm_is_security_group_rule" "allow_22" {
 
 #Ingress 443 Port
 resource "ibm_is_security_group_rule" "allow_443" {
-  depends_on = ["ibm_is_security_group.ckp_security_group"]
-  group      = "${ibm_is_security_group.ckp_security_group.id}"
+  depends_on = [ibm_is_security_group.ckp_security_group]
+  group      = ibm_is_security_group.ckp_security_group.id
   direction  = "inbound"
   remote     = "0.0.0.0/0"
   tcp {
@@ -185,8 +184,8 @@ resource "ibm_is_security_group_rule" "allow_443" {
 
 #Ingress 18200 Ports
 resource "ibm_is_security_group_rule" "allow_18200s" {
-  depends_on = ["ibm_is_security_group.ckp_security_group"]
-  group      = "${ibm_is_security_group.ckp_security_group.id}"
+  depends_on = [ibm_is_security_group.ckp_security_group]
+  group      = ibm_is_security_group.ckp_security_group.id
   direction  = "inbound"
   remote     = "0.0.0.0/0"
   tcp {
@@ -197,8 +196,8 @@ resource "ibm_is_security_group_rule" "allow_18200s" {
 
 #Ingress 18264 Port
 resource "ibm_is_security_group_rule" "allow_18264" {
-  depends_on = ["ibm_is_security_group.ckp_security_group"]
-  group      = "${ibm_is_security_group.ckp_security_group.id}"
+  depends_on = [ibm_is_security_group.ckp_security_group]
+  group      = ibm_is_security_group.ckp_security_group.id
   direction  = "inbound"
   remote     = "0.0.0.0/0"
   tcp {
@@ -209,8 +208,8 @@ resource "ibm_is_security_group_rule" "allow_18264" {
 
 #Ingress 18100 Ports
 resource "ibm_is_security_group_rule" "allow_18100s" {
-  depends_on = ["ibm_is_security_group.ckp_security_group"]
-  group      = "${ibm_is_security_group.ckp_security_group.id}"
+  depends_on = [ibm_is_security_group.ckp_security_group]
+  group      = ibm_is_security_group.ckp_security_group.id
   direction  = "inbound"
   remote     = "0.0.0.0/0"
   tcp {
@@ -221,8 +220,8 @@ resource "ibm_is_security_group_rule" "allow_18100s" {
 
 #Ingress 19009 Port
 resource "ibm_is_security_group_rule" "allow_19009" {
-  depends_on = ["ibm_is_security_group.ckp_security_group"]
-  group      = "${ibm_is_security_group.ckp_security_group.id}"
+  depends_on = [ibm_is_security_group.ckp_security_group]
+  group      = ibm_is_security_group.ckp_security_group.id
   direction  = "inbound"
   remote     = "0.0.0.0/0"
   tcp {
@@ -233,8 +232,8 @@ resource "ibm_is_security_group_rule" "allow_19009" {
 
 #Ingress 257 Port
 resource "ibm_is_security_group_rule" "allow_257" {
-  depends_on = ["ibm_is_security_group.ckp_security_group"]
-  group      = "${ibm_is_security_group.ckp_security_group.id}"
+  depends_on = [ibm_is_security_group.ckp_security_group]
+  group      = ibm_is_security_group.ckp_security_group.id
   direction  = "inbound"
   remote     = "0.0.0.0/0"
   tcp {
@@ -249,21 +248,25 @@ resource "ibm_is_security_group_rule" "allow_257" {
 ##############################################################################
 
 resource "ibm_is_instance" "cp_mgmt_vsi" {
-  depends_on = ["ibm_is_security_group_rule.allow_257", "data.ibm_is_image.cp_mgmt_custom_image"]
-  name    = "${var.VNF_CP-MGMT_Instance}"
-  image   = "${data.ibm_is_image.cp_mgmt_custom_image.id}"
-  profile = "${data.ibm_is_instance_profile.vnf_profile.name}"
-  resource_group = "${data.ibm_resource_group.rg.id}"
+  depends_on = [ibm_is_security_group_rule.allow_257, data.ibm_is_image.cp_mgmt_custom_image]
+  name    = var.VNF_CP-MGMT_Instance
+  image   = ibm_is_image.cp_mgmt_custom_image.id
+  profile = data.ibm_is_instance_profile.vnf_profile.name
+  resource_group = data.ibm_resource_group.rg.id
 
+  #eth0 - Management Interface
   primary_network_interface {
     name   = "eth0"
-    subnet = "${data.ibm_is_subnet.cp_subnet1.id}"
-    security_groups = ["${ibm_is_security_group.ckp_security_group.id}"]
+    subnet = data.ibm_is_subnet.cp_subnet1.id
+    security_groups = [ibm_is_security_group.ckp_security_group.id]
   }
   
-  vpc  = "${data.ibm_is_vpc.cp_vpc.id}"
-  zone = "${data.ibm_is_zone.zone.name}"
-  keys = ["${data.ibm_is_ssh_key.cp_ssh_pub_key.id}"]
+  vpc  = data.ibm_is_vpc.cp_vpc.id
+  zone = data.ibm_is_subnet.cp_subnet1.zone
+  keys = [data.ibm_is_ssh_key.cp_ssh_pub_key.id]
+
+  #Custom UserData
+  user_data = file("user_data")
 
   //User can configure timeouts
   timeouts {
@@ -279,20 +282,20 @@ resource "ibm_is_instance" "cp_mgmt_vsi" {
 #Create and Assoiciate Floating IP Address
 resource "ibm_is_floating_ip" "cp_mgmt_vsi_floatingip" {
   name   = "${var.VNF_CP-MGMT_Instance}-fip"
-  target = "${ibm_is_instance.cp_mgmt_vsi.primary_network_interface.0.id}"
+  target = ibm_is_instance.cp_mgmt_vsi.primary_network_interface.0.id
 }
 
 # Delete checkpoint management custom image from the local user after VSI creation.
 data "external" "delete_custom_image2" {
-  depends_on = ["ibm_is_instance.cp_mgmt_vsi"]
+  depends_on = [ibm_is_instance.cp_mgmt_vsi]
   program    = ["bash", "${path.module}/scripts/delete_custom_image.sh"]
 
   query = {
-    custom_image_id   = "${data.ibm_is_image.cp_mgmt_custom_image.id}"
-    region            = "${var.VPC_Region}"
+    custom_image_id   = data.ibm_is_image.cp_mgmt_custom_image.id
+    region            = var.VPC_Region
   }
 }
 
 output "delete_custom_image2" {
-  value = "${lookup(data.external.delete_custom_image2.result, "custom_image_id")}"
+  value = lookup(data.external.delete_custom_image2.result, "custom_image_id")
 }
