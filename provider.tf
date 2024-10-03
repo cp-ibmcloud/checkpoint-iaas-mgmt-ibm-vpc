f608ec5703be0cab10e065e92660d08b1dfd2dc0
cp-ibmcloud <96487735+cp-ibmcloud@users.noreply.github.com>
2024-10-03 10:12:28 -0400

    Update provider.tf set to version version = "1.69.2"

--- provider.tf
+++ provider.tf
@@ -2,6 +2,7 @@ terraform {
   required_providers {
     ibm = {
       source = "IBM-Cloud/ibm"
+      version = "1.69.2"
       #version = "1.30.2"
       #version = "1.62.0"
       #version = "1.59.0-vnip2.1"
